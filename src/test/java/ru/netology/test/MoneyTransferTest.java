a425c465c6178e276d4546bec6a4ae98699a6b4c
Anna-64 <shurygina6410@gmail.com>
2022-04-12 21:13:22 +0400

    added MoneyTransferTest

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -138,7 +138,7 @@ public class MoneyTransferTest {
     var dashboardPage = verificationPage.validVerify(verifyInfo);
     var firstCardInfo = getFirstCardInfo();
     var secondCardInfo = getSecondCardInfo();
-    int amount = 20000;
+    int amount = 9999;
     var expectedBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo) - amount;
     var expectedBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo) + amount;
     var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
